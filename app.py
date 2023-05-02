c21d2a6487ca6c01755502d00283e860a999ff20
CheoR <cheo.roman+github@gmail.com>
2023-05-02 13:47:14 -0500

    chore: remove unused routes

--- app.py
+++ app.py
@@ -21,54 +21,54 @@ def beer():
 def page_not_found(err):
     return render_template('404.html'), 404
 
-@app.route('/pug')
-def pug():
-    return "<p>PUG</p>"
+# @app.route('/pug')
+# def pug():
+#     return "<p>PUG</p>"
 
-@app.route('/user/<username>')
-def show_user_profile(username):
-    # show the user profile for that user
-    return f'User {escape(username)}'
+# @app.route('/user/<username>')
+# def show_user_profile(username):
+#     # show the user profile for that user
+#     return f'User {escape(username)}'
 
-@app.route('/post/<int:post_id>')
-def show_post(post_id):
-    print(f"got {post_id}")
-    # show the post with the given id, the id is an integer
-    return f'Post {post_id}'
+# @app.route('/post/<int:post_id>')
+# def show_post(post_id):
+#     print(f"got {post_id}")
+#     # show the post with the given id, the id is an integer
+#     return f'Post {post_id}'
 
-@app.route('/path/<path:subpath>')
-def show_subpath(subpath):
-    # show the subpath after /path/
-    return f'Subpath {escape(subpath)}'
+# @app.route('/path/<path:subpath>')
+# def show_subpath(subpath):
+#     # show the subpath after /path/
+#     return f'Subpath {escape(subpath)}'
 
-with app.test_request_context():
-    print(url_for('pug', next='/'))
-    # /pug?next=%2F
+# with app.test_request_context():
+#     print(url_for('pug', next='/'))
+#     # /pug?next=%2F
 
-def do_the_login():
-    print('it was a POST request')
-    return 'it was a POST request'
+# def do_the_login():
+#     print('it was a POST request')
+#     return 'it was a POST request'
 
-def show_the_login_form():
-    print('it was a GET request')
-    return 'it was a POST request'
+# def show_the_login_form():
+#     print('it was a GET request')
+#     return 'it was a POST request'
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        return do_the_login()
-    else:
-        return show_the_login_form()
+# @app.route('/login', methods=['GET', 'POST'])
+# def login():
+#     if request.method == 'POST':
+#         return do_the_login()
+#     else:
+#         return show_the_login_form()
 
-@app.get('/another-login')
-def login_get():
-    return show_the_login_form()
+# @app.get('/another-login')
+# def login_get():
+#     return show_the_login_form()
 
-@app.post('/another-login')
-def login_post():
-    return do_the_login()
+# @app.post('/another-login')
+# def login_post():
+#     return do_the_login()
 
-@app.route('/hello/')
-@app.route('/hello/<name>')
-def hello(name=None):
-    return render_template('hello.html', name=name)
+# @app.route('/hello/')
+# @app.route('/hello/<name>')
+# def hello(name=None):
+#     return render_template('hello.html', name=name)
